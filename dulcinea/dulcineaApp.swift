399e139086f59f89b954db6b3d8ff0f9ef05d1e6
Dominic Mauro <dl.mauro@icloud.com>
2025-06-11 20:26:16 -0400

    Initial Commit

---
+++ dulcinea/dulcineaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dulcineaApp.swift
+//  dulcinea
+//
+//  Created by Dominic Mauro on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct dulcineaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
